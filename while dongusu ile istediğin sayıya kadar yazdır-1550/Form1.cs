334eaf8fcdc00c24895bdb7f57e37e1b1e1284bd
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-11-27 04:04:35 +0300

    Add files via upload

---
+++ while dongusu ile istediğin sayıya kadar yazdır-1550/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace while_dongusu_ile_istediğin_sayıya_kadar_yazdır_1550
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int say = 1;
+            while(say < 10)
+            {
+                listBox1.Items.Add(say);
+                say++;
+            }
+        }
+    }
+}
---
+++ while dongusu ile istediğin sayıya kadar yazdır-1550/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace while_dongusu_ile_istediğin_sayıya_kadar_yazdır_1550
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
